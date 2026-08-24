0016cbc245c5e02fbe70e49d0e90dc92ed72de44
MatthewBollex <matthbolle@gmail.com>
2026-08-24 12:15:11 +0200

    ahhhhhh

--- com.prova.app/src/main/java/com/prova/app/App.java
+++ com.prova.app/src/main/java/com/prova/app/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Ciao Mondo?" );
         System.out.println( "Hola Mundo." );
+        System.out.println( "ahhhhhh" );
     }
 }
