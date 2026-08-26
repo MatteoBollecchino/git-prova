39bb7d623d0e2b4e3cd7b99a939ae067bdbaf061
MatthewBollex <matthbolle@gmail.com>
2026-08-26 11:08:29 +0200

    updated app files

--- com.prova.app/src/main/java/com/prova/app/App.java
+++ com.prova.app/src/main/java/com/prova/app/App.java
@@ -1,20 +1,11 @@
 package com.prova.app;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Ciao Mondo?" );
-        System.out.println( "Io sono Matteo" );
-        System.out.println( "Hola Mundo." );
-        System.out.println( "ahhhhhh" );
-        System.out.println( "Hi" );
-        System.out.println( "Ciao" );
-        System.out.println( "Nuova stampa" );
-    }
+
+	public String sayHello() {
+		return "Hello world";
+	}
+    
 }
--- com.prova.app/src/test/java/com/prova/app/AppTest.java
+++ com.prova.app/src/test/java/com/prova/app/AppTest.java
@@ -1,38 +1,16 @@
 package com.prova.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
+import org.junit.Test;
+
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @Test
+    public void TestApp() {
+    	App app = new App();
+    	assertEquals("Hello world", app.sayHello());
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
 }
