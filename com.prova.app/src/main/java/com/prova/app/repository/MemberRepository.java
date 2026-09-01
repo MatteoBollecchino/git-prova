9afb2c2ec4667203911031312dc8f0123658603d
MatthewBollex <matthbolle@gmail.com>
2026-09-01 19:44:32 +0200

    repository files updated

--- com.prova.app/src/main/java/com/prova/app/repository/MemberRepository.java
+++ com.prova.app/src/main/java/com/prova/app/repository/MemberRepository.java
@@ -3,16 +3,17 @@ package com.prova.app.repository;
 import com.prova.app.model.Member;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface MemberRepository {
 	
 	void save(Member member);
 	
-	Optional<Member> findById(Long id);
+	Optional<Member> findById(UUID id);
 	
 	List<Member> findAll();
 	
-	void deleteById(Long id);
+	void deleteById(UUID id);
 	
 	void clear();
 
--- com.prova.app/src/main/java/com/prova/app/repository/SubscriptionRepository.java
+++ com.prova.app/src/main/java/com/prova/app/repository/SubscriptionRepository.java
@@ -3,16 +3,17 @@ package com.prova.app.repository;
 import com.prova.app.model.Subscription; 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface SubscriptionRepository {
 	
 	void save(Subscription subscription);
 	
-	Optional<Subscription> findById(Long id);
+	Optional<Subscription> findById(UUID id);
 	
 	List<Subscription> findAll();
 	
-	void deleteById(Long id);
+	void deleteById(UUID id);
 	
 	void clear();
 
